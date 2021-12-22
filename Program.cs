f03858d79091558bd6958de3d3ba286a0c3211c0
cezar <cezar@NOTEB>
2021-12-22 02:12:32 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int numFuncionario, numhoras;
+            double valHora, valFinal;
+
+            numFuncionario = int.Parse(Console.ReadLine());
+            numhoras = int.Parse(Console.ReadLine());
+            valHora = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine(numFuncionario);
+            valFinal = numhoras * valHora;
+            Console.WriteLine(valFinal.ToString("F2",CultureInfo.InvariantCulture));
+
+        }
+    }
+}
